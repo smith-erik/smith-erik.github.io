2d7e85f778efb41f8062ab3af43a4d8ff406744b
Erik Smith <eriksmith04@gmail.com>
2017-07-09 14:46:43 +0200

    Added jQuery and rewrote existing JS using jQuery

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var nav2 = document.getElementById('test');
-nav2.onmouseover = showHover;
-nav2.onmouseout = hideHover;
 
-var tooltip = document.getElementById('hovertext');
+$(document).ready(function() {
 
-function showHover() {
-	tooltip.style.display = "block";
-}
+	var tooltip = $(".hovertext")
 
-function hideHover() {
-	tooltip.style.display = "none";
-}
+	$("a.upcoming").hover(function(){
+        tooltip.toggle();
+    }, function() {
+    	tooltip.toggle();
+    });
 
-window.onmousemove = function (e) {
-    var x = e.clientX,
-        y = e.clientY;
-    tooltip.style.top = (y + 20) + 'px';
-    tooltip.style.left = (x + 20) + 'px';
-};
+	$(window).mousemove(function(e) {
+		/* Act on the event */
+	    var x = e.clientX, y = e.clientY;
+	    tooltip.css({
+	    	top: (y + 20) + 'px',
+	    	left: (x + 20) + 'px'
+	    });
+	});
+});

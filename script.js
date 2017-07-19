b61ff9b66938cd54446caff13f3a998f6d8ab6ab
Erik <eriksmith04@gmail.com>
2017-07-19 22:48:57 +0200

    Added beginning of IBAN validator form

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
 $(document).ready(function() {
 
+    $("form[name='ibanform']").submit(function(e) {
+        var iban = $("input[name='iban']", this).val();
+        // alternatively var iban = $(this).find("input[name='iban']").val();
+        alert(iban);
+    });
+
     var tooltip = $(".hovertext")
 
     $("a.upcoming").hover(function(){
@@ -17,4 +23,5 @@ $(document).ready(function() {
             left: (x + 20) + 'px'
         });
     });
+
 });

fddbed3ae8f3c6a6f6ab6484df7b0095eb00f1c4
Erik <eriksmith04@gmail.com>
2017-07-19 21:32:09 +0200

    Converted indentation to spaces

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
 $(document).ready(function() {
 
-	var tooltip = $(".hovertext")
+    var tooltip = $(".hovertext")
 
-	$("a.upcoming").hover(function(){
+    $("a.upcoming").hover(function(){
         tooltip.toggle();
     }, function() {
-    	tooltip.toggle();
+        tooltip.toggle();
     });
 
-	$(window).mousemove(function(e) {
-		/* Act on the event */
-	    var x = e.clientX, y = e.clientY;
-	    tooltip.css({
-	    	top: (y + 20) + 'px',
-	    	left: (x + 20) + 'px'
-	    });
-	});
+    $(window).mousemove(function(e) {
+        /* Act on the event */
+        var x = e.clientX, y = e.clientY;
+        tooltip.css({
+            top: (y + 20) + 'px',
+            left: (x + 20) + 'px'
+        });
+    });
 });
